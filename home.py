ea29340a8b1742963d0c62cac5191ccd0b04bee2
lizi1938848240 <128972039+lizi1938848240@users.noreply.github.com>
2023-07-02 21:27:34 +0800

    Add files via upload

---
+++ home.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  2 19:35:23 2023
+
+@author: lenovo
+"""
+
+import streamlit as st
+st.title("X作物适宜区分析")
+st.write("这里可以通过气温、降水、海拔、坡向、沟谷线缓冲区对X作物适宜区进行分析")
+st.write("X作物可以销售到餐厅、超市等场所，本程序可以进行路径规划分析")
+st.write("可以通过机器学习判断是否适宜")
+st.write("技术路线图如下：")
